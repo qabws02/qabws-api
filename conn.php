aa17e5dd5c4e5d5baeda7f9a0d50f35a2544ff81
qabws02 <qabwsb02@gmail.com>
2026-05-07 22:54:22 +0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,30 +1,26 @@
 <?php
+header("Content-Type: application/json");
 
-header("Content-Type: application/json; charset=utf8mb4");
-
-$host = "sql304.infinityfree.com";
-$db   = "if0_40839769_info39769";
-$user = "if0_40839769";
-$pass = "PIdBUzww9FqCAs";
+$host = "mysql-cc1c3ad-qabwsb02-598d.k.aivencloud.com";
+$port = 12495;
+$db   = "defaultdb";
+$user = "avnadmin";
+$pass = "PUT_YOUR_PASSWORD_HERE";
 
 try {
     $conn = new PDO(
-        "mysql:host=$host;dbname=$db;charset=utf8mb4",
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
         $user,
-        $pass
+        $pass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
     );
 
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 } catch (PDOException $e) {
-    http_response_code(500);
-
-    echo json_encode([
+    die(json_encode([
         "status" => false,
-        "message" => "Database connection failed",
         "error" => $e->getMessage()
-    ]);
-
-    exit();
+    ]));
 }
 ?>
