eb751d786958795e77054cd0cdde9cc39e5c58a7
qabws02 <qabwsb02@gmail.com>
2026-05-07 18:20:57 +0300

    login.php

--- login.php
+++ login.php
@@ -1,8 +1,40 @@
 <?php
-header("Content-Type: application/json");
 
-echo json_encode([
-    "status" => true,
-    "message" => "API connected"
-]);
+header("Content-Type: application/json; charset=utf8mb4");
+
+// إعدادات قاعدة البيانات
+$host = "sql304.infinityfree.com";
+$user = "if0_40839769";
+$pass = "PIdBUzww9FqCAs";
+$db   = "if0_40839769_info39769";
+
+// إنشاء الاتصال
+$conn = new mysqli($host, $user, $pass, $db);
+
+// التحقق من الخطأ
+if ($conn->connect_error) {
+    http_response_code(500);
+
+    echo json_encode([
+        "status" => false,
+        "message" => "Database connection failed",
+        "error" => $conn->connect_error
+    ]);
+
+    exit();
+}
+
+// ضبط الترميز
+if (!$conn->set_charset("utf8mb4")) {
+    http_response_code(500);
+
+    echo json_encode([
+        "status" => false,
+        "message" => "Charset error",
+        "error" => $conn->error
+    ]);
+
+    exit();
+}
+
 ?>
