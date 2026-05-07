af1de658b2cf25601a3d145fe1054907af0807f8
qabws02 <qabwsb02@gmail.com>
2026-05-07 18:41:14 +0300

    Update get_users.php

--- get_users.php
+++ get_users.php
@@ -1,25 +1,15 @@
 <?php
 header("Content-Type: application/json");
+
 include "conn.php";
 
-$sql = "SELECT id_users, name, email, phn, img, addrss FROM users";
-$result = $conn->query($sql);
+$stmt = $conn->prepare("SELECT id_users, name, email FROM users");
+$stmt->execute();
 
-$users = [];
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $users[] = $row;
-    }
-
-    echo json_encode([
-        "status" => "success",
-        "data" => $users
-    ]);
-} else {
-    echo json_encode([
-        "status" => "empty",
-        "data" => []
-    ]);
-}
+echo json_encode([
+    "status" => true,
+    "data" => $users
+]);
 ?>
